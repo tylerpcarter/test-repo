abd21389474e7faa3d7df450435e3e7b3bda4b2b
Tyler <tylercartergames@gmail.com>
2022-04-04 12:33:54 -0400

    added promise.all for swapi first 2 pages of people.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const fetchSWAPI = async () => {
+  const response = await fetch(`https://swapi.dev/api/people/?page=1`);
+  const jsonResponse = await response.json();
+  return jsonResponse;
+  }
+
+  const fetchPage2 = async() => {
+    const response2 = await fetch(`https://swapi.dev/api/people/?page=2`);
+    const jsonResponse2 = await response2.json();
+    return jsonResponse2;
+  }
+
+
+  useEffect(() => {
+    Promise.all([fetchSWAPI(), fetchPage2()]).then(values => console.log(values));
+  }, []);
+  
+
   return (
     <div className="App">
       <h1>Hello world!</h1>
